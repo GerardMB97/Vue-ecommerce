f0e5a48d86fc4d02d8cc56095f10930a949681f9
Gerard Mangues <gerardcoding@gmail.com>
2021-04-20 12:55:08 +0200

    Add header component basic mobile structure

---
+++ src/components/Header/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+      <div class="header__top">
+
+      </div>
+      <div class="header__middle">
+
+      </div>
+      <div class="header__bottom">
+
+      </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Header extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+@import './header.scss';
+
+</style>
