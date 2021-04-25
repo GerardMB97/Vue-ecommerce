08a25b93bf4c209acf9dac9828465d4ddfbfda74
Gerard Mangues <gerardcoding@gmail.com>
2021-04-25 13:24:23 +0200

    Add Product Detail view to routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
 import Products from '../views/Products.vue';
+import ProductDetail from '../views/ProductDetail.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   { path: '/', component: Home },
-  { path: '/:category', component: Products },
+  { path: '/products/:category', component: Products },
+  { path: '/products/:id' , component: ProductDetail},
 ];
 
 const router = new VueRouter({
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Header from '../components/Header/index.vue';
+import Footer from '../components/Footer/index.vue';
+
+@Component({
+  components: {
+    Header,
+    Footer,
+  },
+})
+export default class Products extends Vue {
+
+}
+</script>
