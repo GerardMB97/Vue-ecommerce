5602ce0c7b409c13d4e6c7007f4ea9d12a35e987
Gerard Mangues <gerardcoding@gmail.com>
2021-04-25 18:30:06 +0200

    Add product detail component styles for mobile and desktop versions

--- src/components/ProductDetailComponent/index.vue
+++ src/components/ProductDetailComponent/index.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
-    <article v-if="product" className="flex container__product">
-      <div className="product__image">
+    <article v-if="product" class= "container__product">
+      <div class="product__image">
         <img :src="product.image" alt="Product detail" />
       </div>
-      <div className="flex product__text">
+      <div class="flex product__text">
         <h5>{{product.category}}</h5>
         <h3>{{product.title}}</h3>
-        <p className="price">
-          {{product.price}}
-          {' '}
-          €
-        </p>
+        <p class="price">{{product.price}} €</p>
         <p>{{product.description}}</p>
-        <p className="quantity__title">QUANTITAT</p>
+        <p class="quantity__title">QUANTITY</p>
+        <input type="number" v-model="quantity">
+        <button>ADD</button>
       </div>
     </article>
   </div>
@@ -40,9 +38,14 @@ export default class ProductDetailComponent extends Vue {
 
   id!: string
 
+  quantity = 0;
+
   get product():Product {
     console.log(this.id);
     return this.products.find((product) => product.id === +this.id);
   }
 }
 </script>
+<style lang="scss" scoped>
+@import './productDetailComponents.scss';
+</style>
