2deb53d79b75f7f6c68f16e835517bfe58ea629d
Gerard Mangues <gerardcoding@gmail.com>
2021-04-26 12:26:07 +0200

    Add Cart view to routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
 import Products from '../views/Products.vue';
 import ProductDetail from '../views/ProductDetail.vue';
+import Cart from '../views/Cart.vue';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,7 @@ const routes: Array<RouteConfig> = [
   { path: '/', component: Home },
   { path: '/products/:category', component: Products },
   { path: '/product/:id', component: ProductDetail },
+  { path: './cart', component: Cart },
 ];
 
 const router = new VueRouter({
